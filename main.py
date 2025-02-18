158e363b33fe9654fde104736d5352f39b432bcc
danangtri <58099677+danangzx@users.noreply.github.com>
2025-02-19 00:21:28 +0700

    Update main.py
    Add Feature Count Pixels

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from kivy.lang import Builder
+
 from kivymd.app import MDApp
 from kivymd.uix.button import MDRaisedButton
 from kivymd.uix.boxlayout import MDBoxLayout
@@ -21,37 +23,45 @@ from detectron2.data import MetadataCatalog, DatasetCatalog
 from detectron2.data.datasets import register_coco_instances
 from detectron2.data import DatasetCatalog, MetadataCatalog
 
+KV = '''
+BoxLayout:
+    orientation: 'vertical'
+
+    MDFloatLayout:
+        Image:
+            id: img
+            pos_hint: {"center_x": .5, "center_y": .52}
+            
+
+    MDBoxLayout:
+        orientation: 'horizontal'
+        padding: 5
+        spacing: 5
+        size_hint: 1, 0.12
+        MDRaisedButton:
+            text: "Upload"
+            size_hint: None, None
+            size: "150dp", "40dp"
+            on_release:
+                app.upload_image()
+
+        MDRaisedButton:
+            text: "Detect"
+            size_hint: None, None
+            size: "150dp", "40dp"
+            on_release:
+                app.load_image()
+
+        MDLabel:
+            id: value_label
+            text: ""
+            size_hint_y: None
+            height: "40dp"
+'''
+
 class DetectionApp(MDApp):
+
     def build(self):
-        self.layout = MDBoxLayout(
-            orientation="vertical",
-            spacing=20,
-            padding=20
-        )
-        self.upload_button = MDRaisedButton(
-            text="Upload",
-            on_release=self.upload_image
-        )
-        self.press_button = MDRaisedButton(
-            text="Proses",
-            on_release=self.load_image
-        )
-        self.image = Image()
-        self.layout.add_widget(self.image)
-        self.layout.add_widget(self.upload_button)
-        self.layout.add_widget(self.press_button)
-        return self.layout
-
-    def upload_image(self,instance):
-        filechooser.open_file(on_selection=self.selected)
-    
-    def selected(self,selection):
-        self.image_upload = selection[0]
-        image_source = selection[0]
-        if selection:
-            self.image.source = image_source
-
-    def load_image(self,instance):
         self.road_damage_metadata = MetadataCatalog.get("road_damage")
         self.road_damage_metadata.thing_classes = ["Road-damage", "alligator cracking","lateral cracking","longitudinal cracking","pothole"]
 
@@ -74,32 +84,193 @@ class DetectionApp(MDApp):
         self.cfg.MODEL.DEVICE = "cpu"
 
         self.predictor = DefaultPredictor(self.cfg)
+        return Builder.load_string(KV)
+    
+
+
+    def nilai_persen(self,nilai):
+        nilai_dibulatkan = round(nilai, 2)
+        nilai_persen = nilai_dibulatkan * 100
+        teks_nilai = f"{int(nilai_persen)}"
+        return teks_nilai
+    
+    def ubah_ke_integer(self,arr):
+        return [[int(x) for x in row] for row in arr]
+    
+    def hitung_objek_asli(self,koordinat_deteksi):
+
+        x1,y1,x2,y2 = koordinat_deteksi
+        
+        luas = np.sqrt((x2-x1)**2 * (y2-y1)**2)
+        
+        return luas
+    
+    def get_2d_array_string(self,arr1):
+        formatted_text = ""
+        jumlah_lubang = 0
+        jumlah_retak_memanjang = 0
+        jumlah_retak_melintang = 0
+        jumlah_retak_buaya = 0
+        luas_lubang = 0
+        luas_retak_memanjang = 0
+        luas_retak_melintang = 0
+        luas_retak_buaya = 0
+        
+        
+        #menghitung kelas yang terdeteksi
+        for item in arr1:
+            jenis = item[0]
+            nilai = int(item[1])
+            if jenis == 'lubang':
+                jumlah_lubang += 1
+                luas_lubang += nilai
+            elif jenis == 'retak memanjang':
+                jumlah_retak_memanjang += 1
+                luas_retak_memanjang += nilai
+            elif jenis == 'retak melintang':
+                jumlah_retak_melintang += 1
+                luas_retak_melintang += nilai
+            elif jenis == 'retak buaya':
+                jumlah_retak_buaya += 1
+                luas_retak_buaya += nilai
+        
+        #
+        jumlah_array = []
+        if jumlah_lubang > 0:
+            jumlah_array.append((f"Jumlah lubang = {jumlah_lubang},                         total luas lubang ={luas_lubang}px\n"))
+        if jumlah_retak_memanjang > 0:
+            jumlah_array.append((f"Jumlah retak memanjang = {jumlah_retak_memanjang},       total luas retak memanjang ={luas_retak_memanjang}px\n"))
+        if jumlah_retak_melintang > 0:
+            jumlah_array.append((f"Jumlah retak melintang = {jumlah_retak_melintang},          total luas melintang ={luas_retak_melintang}px\n"))
+        if jumlah_retak_buaya > 0:
+            jumlah_array.append((f"Jumlah retak buaya = {jumlah_retak_buaya},                 total luas retak buaya ={luas_retak_buaya}px\n"))
+        
+        #menampilkan pada layar
+        for item in jumlah_array:
+            formatted_text += item            
+            
+        self.root.ids.value_label.text = formatted_text
+
+    def buat_kotak(self,koordinat_int, total_ukur,teks_foto):        
+        x1,y1,x2,y2 = koordinat_int
+
+        self.tinggi, self.lebar, _ = self.foto.shape
+        
+
+        if self.tinggi <=800 or self.lebar <= 800 :
+
+            # Gambar kotak pada objek
+            # cv2.rectangle(foto, (x1,y1), (x2,y2), (0, 255, 0), 2)
+            if( total_ukur <= 1000 ):
+                #hijau
+                cv2.rectangle(self.foto, (x1,y1), (x2,y2), (0, 255, 0), 2)
+                cv2.putText(self.foto, teks_foto, (x1, y1-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+            elif (total_ukur <= 5000):
+                #kuning
+                cv2.rectangle(self.foto, (x1,y1), (x2,y2), (0, 255, 255), 2)
+                cv2.putText(self.foto, teks_foto, (x1, y1-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 2)
+            else:
+                #merah
+                cv2.rectangle(self.foto, (x1,y1), (x2,y2), (0, 0, 255), 2)
+                cv2.putText(self.foto, teks_foto, (x1, y1-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)            
+
+        else:
+
+            if( total_ukur <= 50000 ):
+                #hijau
+                cv2.rectangle(self.foto, (x1,y1), (x2,y2), (0, 255, 0), 10)
+                cv2.putText(self.foto, teks_foto, (x1, y1-10), cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 255, 0), 5)
+            elif (total_ukur <= 500000):
+                #kuning
+                cv2.rectangle(self.foto, (x1,y1), (x2,y2), (0, 255, 255), 10)
+                cv2.putText(self.foto, teks_foto, (x1, y1-10), cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 255, 255), 5)
+            else:
+                #merah
+                cv2.rectangle(self.foto, (x1,y1), (x2,y2), (0, 0, 255), 10)
+                cv2.putText(self.foto, teks_foto, (x1, y1-10), cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 0, 255), 5)
+                
+
+    
+    def load_image(self):
+
+        #nilai array teks yang akan ditampilkan
+        teks = []
+
+        #nilai skor dari prediksi menjadi array
+        nilai_skor = []
+
+        #variabel hasil ukuran
+        hasil = []
 
         # Baca gambar menggunakan OpenCV
         image = cv2.imread(self.image_upload)  # Ganti dengan lokasi gambar Anda
+        self.foto = image
         predictions = self.predictor(image)
 
-        viz = Visualizer(image[:, :, ::-1], metadata = self.road_damage_metadata, )
-        
-        output = viz.draw_instance_predictions(predictions["instances"].to("cpu"))
+        # Mengubah nilai float menjadi integer
+        #ubah pred_box ke list
+        pred_box = predictions["instances"].pred_boxes.tensor.tolist()
+        pred_box_int = self.ubah_ke_integer(pred_box)
 
-        img_output = output.get_image()[:,:,::-1]
+        #nilai kelas menjadi array
+        class_box = predictions["instances"].pred_classes.tolist()
+        for i in range(len(class_box)):
+            if class_box[i] == 1:
+                class_box[i] = "retak buaya"
+            if class_box[i] == 2:
+                class_box[i] = "retak melintang"
+            if class_box[i] == 3:
+                class_box[i] = "retak memanjang"
+            if class_box[i] == 4:
+                class_box[i] = "lubang"
+
+        #nilai skor dari prediksi menjadi array
+        scores_box = predictions["instances"].scores.tolist()
+
+        for i in range(len(scores_box)):
+            nilai_skor.append(self.nilai_persen(scores_box[i]))
+
+        for i in range(len(pred_box)):
+            hasil.append(self.hitung_objek_asli(pred_box[i]))
+        
+        for i in range(len(class_box)):
+            # Menentukan teks yang akan ditampilkan
+            teks.append(f"{class_box[i]}= {nilai_skor[i]}%")
+
+        # Menggabungkan dua array menjadi array 2 dimensi
+        combined_2d_array = [[class_box[i], hasil[i]] for i in range(min(len(class_box), len(hasil)))]
+
+        self.get_2d_array_string(combined_2d_array)
+
+        # mencetak hasil
+        for j in range(len(pred_box_int)):
+            self.buat_kotak(pred_box_int[j],hasil[j],teks[j])
 
         # Pastikan gambar berhasil dimuat
-        if img_output is not None:
+        # image
+        if image is not None:
             # Konversi dari BGR ke RGB
-            image_rgb = cv2.cvtColor(img_output, cv2.COLOR_BGR2RGB)
+            image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
             # Buat objek Texture dari gambar
-            texture = Texture.create(size=(img_output.shape[1], img_output.shape[0]))
+            texture = Texture.create(size=(image.shape[1], image.shape[0]))
             texture.flip_vertical()
             texture.blit_buffer(image_rgb.tobytes(), colorfmt='rgb', bufferfmt='ubyte')
 
             # Tampilkan gambar di Kivy
-            self.image.texture = texture
+            self.root.ids.img.texture = texture
         else:
             print("Gagal memuat gambar.")
 
+    def upload_image(self):
+        filechooser.open_file(on_selection=self.selected)
+    
+    def selected(self,selection):
+        self.image_upload = selection[0]
+        image_source = selection[0]
+        if selection:
+            self.root.ids.img.source = image_source
+
+
 if __name__ == '__main__':
-    app = DetectionApp()
-    app.run()
+    DetectionApp().run()
